ca626da00c16dcefa248bf498dd1918572279010
eugenet <eugenetang_@hotmail.com>
2017-02-12 21:35:52 -0800

    Added drawVerticleLine method and test.

--- src/TriangleExercises.java
+++ src/TriangleExercises.java
@@ -1,6 +1,3 @@
-/**
- * Created by eugenet on 2/12/17.
- */
 public class TriangleExercises {
     public String easiestExerciseEver() {
         return "*";
@@ -11,6 +8,22 @@ public class TriangleExercises {
 
         for(int i = 0; i < numAstrx; ++i) {
             result += "*";
+        }
+
+        return result;
+    }
+
+    public String drawVerticleLine(int numAstrx) {
+        String result = "";
+
+        for(int i = 0; i < numAstrx; ++i) {
+            //This just does away with newline for last * so we
+            //don't get an extra newline and can use println()
+            if(i == numAstrx - 1) {
+                result += "*";
+                break;
+            }
+            result += "*\n";
         }
 
         return result;
--- tests/TriangleExercisesTest.java
+++ tests/TriangleExercisesTest.java
@@ -3,16 +3,20 @@ import static org.junit.Assert.*;
 
 public class TriangleExercisesTest {
     @Test
-    public void easiestExerciseEverTest() {
+    public void testEasiestExerciseEver() {
         TriangleExercises tri = new TriangleExercises();
         assertEquals("*", tri.easiestExerciseEver());
     }
 
     @Test
-    public void drawHorizontalLineTest() {
+    public void testDrawHorizontalLine() {
         TriangleExercises tri = new TriangleExercises();
         assertEquals("********", tri.drawHorizontalLine(8));
     }
 
-    
+    @Test
+    public void testDrawVerticleLine() {
+        TriangleExercises tri = new TriangleExercises();
+        assertEquals("*\n*\n*", tri.drawVerticleLine(3));
+    }
 }
